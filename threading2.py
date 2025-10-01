e414cd14584826007de4fffce43214d3e7deb34a
Onyeacholem Ifeanyi Joshua <45284393+Gjeotech@users.noreply.github.com>
2025-10-01 12:42:51 +0100

    Update threading2.py

--- threading2.py
+++ threading2.py
@@ -1,24 +1,4 @@
-#Aysch threading task
-
-# import asyncio
-#
-# async def worker(name):
-#     print(f"Worker {name} starting")
-#     await asyncio.sleep(5)
-#     print(f"Worker {name} finished")
-#
-# async def task():
-#     task1 = asyncio.create_task(worker("A"))
-#     task2 = asyncio.create_task(worker("B"))
-#
-#     await task1
-#     await task2
-#
-# asyncio.run(task())
-#
-# print("Both threads completed")
-
-
+#Asynch threading
 
 import asyncio
 
@@ -39,31 +19,3 @@ asyncio.run(task())
 
 print('Both threading completed successfully')
 
-
-"""
-🧪 Topic	🛠️ Mini Project Idea
-Threading	Simulate multiple file downloads
-Asyncio	Async web scraper or countdown timers
-Synchronization	Bank account simulation (thread-safe)
-aiohttp (async HTTP)	Fetch data from multiple websites
-Thread + Async Combo	GUI app that uses async in background
-
-import asyncio → A module
-
-It gives you access to Python’s asynchronous I/O framework.
-
-It includes things like:
-
-asyncio.run() to run async code
-
-asyncio.sleep() for non-blocking delays
-
-asyncio.create_task() for concurrent tasks
-
-Networking tools, queues, and more
-
-=======================
-Keyword/Module	What it is	Purpose
-import asyncio	✅ A Python module (library)	Provides tools for writing asynchronous code
-async	✅ A Python keyword (syntax)	Used to define an asynchronous function
-"""
